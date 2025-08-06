1e0fcc6afd2f43c47cf9b8674a013e737ced7daf
Justin Menegoni <justin.menegoni@datadoghq.com>
2025-08-06 14:48:56 +1000

    new vulns added

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dd-trace').init({
 });
 
 const bunyan = require('bunyan');
-const logger = bunyan.createLogger({ name: 'runtime-sca', level: 'debug' });
+const logger = bunyan.createLogger({ name: 'static-sca', level: 'debug' });
 
 const express = require('express');
 const _ = require('lodash');
@@ -24,10 +24,71 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Here are the new
-// PLEASE UNCOMMENT THE BELOW
+// Here are the new vulnerabilities. You can recomment them using /**/ 
 
 // Command Injection via child_process.exec
+const { exec } = require('child_process');
+// exploit via /api/exec?cmd=ls in the queryparams
+app.get('/api/exec', (req, res) => {
+  const cmd = req.query.cmd;
+  if (!cmd) return res.status(400).send('Missing "cmd"');
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) return res.status(500).send(stderr);
+    res.send(stdout);
+  });
+});
+
+//ReDoS via validator package
+const validator = require('validator');
+// exploit via /api/validate?email=aaaaaaaaaaaaaa!@example.com in queryparams
+app.get('/api/validate', (req, res) => {
+  const email = req.query.email;
+  const isValid = validator.isEmail(email || '');
+  res.send(`Is valid email: ${isValid}`);
+});
+
+//Insecure deserialization via node-serialize
+const serialize = require('node-serialize');
+//data={"exploit":"_$$ND_FUNC$$_function(){require('child_process').exec('ls')}"} 
+app.get('/api/deserialize', (req, res) => {
+  const payload = req.query.data;
+  try {
+    const obj = serialize.unserialize(payload);
+    res.json(obj);
+  } catch (e) {
+    res.status(500).send('Failed to deserialize');
+  }
+});
+
+//Directory Traversal via File Upload (express)
+
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+// Upload a file with a name like ../../etc/passwd.
+app.post('/api/upload', (req, res) => {
+  const file = req.files?.file;
+  if (!file) return res.status(400).send('No file uploaded');
+  file.mv(`uploads/${file.name}`, (err) => {
+    if (err) return res.status(500).send(err.message);
+    res.send('File uploaded');
+  });
+});
+
+// Unsafe YAML Parsing
+const yaml = require('js-yaml');
+/*
+exploit via:
+  !!js/function >
+  function() { require('child_process').exec('ls', console.log) }
+*/
+app.post('/api/yaml', express.text({ type: 'text/yaml' }), (req, res) => {
+  try {
+    const parsed = yaml.load(req.body);
+    res.json(parsed);
+  } catch {
+    res.status(400).send('Invalid YAML');
+  }
+});
 
 /*
 <------------------------------------------------------------------------------------------------------------------------------->
